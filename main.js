f4aa2abe3984fe514b8af56b0e0db9d028e74962
Abdelilah Elhaddad <elhaddadabdelilah@gmail.com>
2020-11-16 19:06:16 +0100

    :fire: :art: First Commit  Desktop Layout :sparkles: :globe_with_meridians:

---
+++ main.js
@@ -0,0 +1,22 @@
+let form = document.getElementById("form");
+let email = document.getElementById("email");
+
+form.addEventListener('submit', e=> {
+  e.preventDefault();
+  EmailChecker();
+})
+
+function EmailChecker(){
+  let Email = email.value;
+  if(!isValid(Email)){
+    document.getElementById("error").textContent = "Please enter a valid email";
+  }
+  else{
+    document.getElementById("error").textContent = "";
+  }
+}
+
+function isValid(Email) {
+	const regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return regx.test(String(Email).toLowerCase());
+}
